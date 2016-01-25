3b77669ff8952d0d469facd947affa7ce4a05807
tanbowensg <bowen.tan@daocloud.io>
2016-01-25 17:28:53 +0800

    完美支持顺序异步验证

--- src/angular-validation-directive.js
+++ src/angular-validation-directive.js
@@ -1,69 +1,157 @@
 angular.module('daoValidAngular')
-.directive('daoValidOld',['DaoValidRules',
-  function(DaoValidRules) {
+  .directive('daoValid', ['DaoValidRules', function(DaoValidRules) {
     var validate = ValidateFactory()
 
     function ValidateFactory() {
       var obj = {}
 
-      obj.validate = function(data) {
-        var result = true
-        var validator, i, j
-        var msg = {}
+      obj.result = {
+        valid: true,
+        msg: {}
+      }
 
-        for (i = 0; i < data.length; i++) {
+      obj.getValidators = function(data) {
+        var i
+        //分为同步验证函数队列和异步验证函数队列
+        obj.validators = []
+        obj.aValidators = []
 
-          for (j = data[i].validators.length - 1; j >= 0; j--) {
+        for (i = 0; i < data.rules.length; i++) {
 
-            try {
-              validator = DaoValidRules[data[i].validators[j]].validate
-            } catch (e) {
-              console.warn('no validator called' + data[i].validators[j])
-              return false
-            }
+          try {
 
-            if (validator(data[i].value)) {
-              continue
+            if (DaoValidRules[data.rules[i]].async) {
+              obj.aValidators.push(DaoValidRules[data.rules[i]])
             } else {
-              result = false
-              msg[data[i].key] = []
-              msg[data[i].key].push(data[i].name + DaoValidRules[data[i].validators[j]].msg)
+              obj.validators.push(DaoValidRules[data.rules[i]])
             }
-          }
-        }
 
-        if (!result) {
-          obj.result = {
-            valid: result,
-            msg: msg
-          }
-        }
+          } catch (e) {
 
-        if (result) {
-          obj.result = {
-            valid: result,
-          }
-        }
+            console.warn('no validator called' + data.rules[i])
+            return false
 
-        return obj
+          }
+
+        }
       }
 
-      obj.success = function(success) {
+      obj.syncValidate = function(data) {
+        //同步的就正常运行
+        for (var i = 0; i < obj.validators.length; i++) {
+
+          var validator = obj.validators[i]
+
+          if (validator.validate(data.value)) {
+            obj.result.msg[data.key] = ""
+
+          } else {
+
+            obj.result.valid = false
+            obj.result.msg[data.key] = data.name + validator.msg
+
+            break
+          }
+
+        }
+
+      }
+
+      obj.asyncValidate = function(data, callback) {
+
+        if (obj.aValidators.length === 0) {
+          return
+        }
+
+        var promise
+
+        for (var i = 0; i < obj.aValidators.length; i++) {
+
+          var aValidator = obj.aValidators[i]
+
+          if (obj.result.valid) {
+
+            if (i === 0) {
+
+              promise = aValidator.validate(data.value)
+
+            } else {
+
+              promise = promise
+                .then(function(res) {
+
+                  if (res.valid) {
+                    return aValidator.validate(res.str)
+                  } else {
+                    return res
+                  }
+
+                })
+
+            }
+
+            if (i === obj.aValidators.length - 1) {
+              promise
+                .then(function(res) {
+
+                  if (res.valid === false && obj.result.valid) {
+
+                    obj.result.valid = false
+                    obj.result.msg[data.key] = data.name + res.msg
+
+                  }
+
+                  if (callback) {
+                    callback()
+                  }
+
+                })
+            }
+
+          }
+
+        }
+
+        return obj.result
+      }
+
+      obj.validate = function(data, success, fail) {
+
+        var validator, i, j
+
+        obj.result.valid = true
+
+        obj.getValidators(data)
+
+        obj.syncValidate(data)
+
+        //仅当同步验证通过时，才会继续异步验证
         if (obj.result.valid) {
-          if (success) {
-            success(obj.result)
-          }
-        }
-        return obj
-      }
 
-      obj.fail = function(fail) {
-        if (!obj.result.valid) {
-          if (fail) {
+          obj.asyncValidate(data, function() {
+
+            if (success && obj.result.valid === true) {
+              success(obj.result)
+            } else if (fail && obj.result.valid === false) {
+              fail(obj.result)
+            }
+
+          })
+
+        } else {
+
+          if (success && obj.result.valid === true) {
+            success(obj.result)
+          } else if (fail && obj.result.valid === false) {
             fail(obj.result)
           }
+
         }
-        return obj
+
+      }
+
+      obj.runCallBack = function(success, fail) {
+
       }
 
       return obj.validate
@@ -79,145 +167,43 @@ angular.module('daoValidAngular')
       link: function($scope, ele) {
         var parent = ele[0].parentElement
         var alert = document.createElement("span")
+        //dirty用来判断用户是否输入过。
+        var dirty = 0
+        alert.className = "text-danger"
 
         $scope.valid = false
 
         $scope.$watch("value", function() {
-          validate([{
+          dirty++
+          $scope.valid = false
+
+          validate({
             name: $scope.name,
             key: "key",
             value: $scope.value,
-            validators: $scope.rule.split(',')
-          }])
-          .success(function(res) {
-            try{
-              parent.removeChild(alert)
-            } catch (e){}
-            $scope.valid = true
-          })
-          .fail(function(rej) {
-            if (ele[0].className.indexOf("ng-dirty")!==-1) {
-              $scope.valid = false
-              alert.innerHTML = rej.msg.key[0]
-              parent.appendChild(alert)
-            }
-          })
-        })
-      }
-    };
-}])
+            rules: $scope.rule.split(',')
+          }, function(res) {
 
-.directive('daoValid', ['DaoValidRules',function(DaoValidRules) {
-  var validate = ValidateFactory()
-
-  function ValidateFactory() {
-    var obj = {}
-
-    window.theresult=obj.result={
-      valid:true,
-      msg:{}
-    }
-
-    obj.asyncValidate = function(data,validator,success,fail){
-      window.strange=obj.result
-      validator.validate(
-        data.value,
-        function(){
-          //success callback
-          if (obj.result.valid===true&&success) {
-            success(obj.result)
-          }
-        },
-        function(){
-          obj.result.valid=false
-          obj.result.msg[data.key] = data.name + validator.msg
-
-          //fail callback
-          if (fail) {
-            fail(obj.result)
-          }
-        })
-    }
-
-    obj.runCallBack = function(success,fail){
-      if(success&&obj.result.valid===true){
-        success(obj.result)
-      } else if (fail&&obj.result.valid===false){
-        fail(obj.result)
-      }
-    }
-    
-    obj.validate = function(data,success,fail) {
-      var validator, i, j
-      obj.result.valid=true
-
-      for (i = 0; i < data.length; i++) {
-
-        for (j = data[i].validators.length - 1; j >= 0; j--) {
-
-          try {
-            validator = DaoValidRules[data[i].validators[j]]
-          } catch (e) {
-            console.warn('no validator called' + data[i].validators[j])
-            return false
-          }
-          //异步验证，如果发现这个是异步的话，那这个验证就不参与验证队列，而是单独运行，直到验证完毕以后，再单独执行一次回调
-          //但是要保证这个异步验证的结果不会覆盖掉其他同步验证的result和msg
-          //
-          if(validator.async===true){
-            obj.asyncValidate(data[i],validator,success,fail)
-            continue
-          }
-
-          //同步验证
-          if (validator.validate(data[i].value)) {
-            obj.result.msg[data[i].key]=""
-          } else {
-            obj.result.valid = false
-            obj.result.msg[data[i].key]=data[i].name + DaoValidRules[data[i].validators[j]].msg
-            break
-          }
-        }
-      }
-
-      obj.runCallBack(success,fail)
-    }
-    return obj.validate
-  }
-
-  return {
-    scope: {
-      name: "@daoValidName",
-      rule: "@daoValidRule",
-      value: "=ngModel",
-      valid: "=daoValidToggle",
-    },
-    link: function($scope, ele) {
-      var parent = ele[0].parentElement
-      var alert = document.createElement("span")
-      alert.className = "text-danger"
-
-      $scope.valid = false
-
-      $scope.$watch("value", function() {
-        validate([{
-            name: $scope.name,
-            key: "key",
-            value: $scope.value,
-            validators: $scope.rule.split(',')
-          }], function(res) {
             try {
+
               parent.removeChild(alert)
+
             } catch (e) {}
+
             $scope.valid = true
+
           }, function(rej) {
-            if (ele[0].className.indexOf("ng-dirty") !== -1) {
+
+            if (dirty>=2) {
+
               $scope.valid = false
               alert.innerHTML = rej.msg.key
               parent.appendChild(alert)
+
             }
+
           })
         })
       }
     };
-}])
+  }])
--- src/angular-validation-rules.js
+++ src/angular-validation-rules.js
@@ -1,38 +1,95 @@
 angular.module('daoValidAngular')
-.factory('DaoValidRules', function(){
-    var rules={}
+  .factory('DaoValidRules', function() {
+    var rules = {}
       // Validation Rules Here--------------------------------------
-      rules.notEmpty = {
-        msg: " can not be empty.",
-        validate: function(str) {
-          return str !== undefined && str.trim() !== ''
-        }
+    rules.notEmpty = {
+      msg: " can not be Empty",
+      validate: function(str) {
+        return str !== undefined && str.trim() !== ''
       }
+    }
 
-      rules.url = {
-        msg:" must be URL.",
-        validate: function(str){
-          var urlRegex = new RegExp("^http\:\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$")
-          return urlRegex.test(str)
-        }
+    rules.url = {
+      msg: " must be an URL",
+      validate: function(str) {
+        var urlRegex = new RegExp("^http\:\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$")
+        return urlRegex.test(str)
       }
+    }
 
-      rules.ipv4 = {
-        msg: " must be IPv4",
-        validate: function(str) {
-          var ipv4Regex = new RegExp("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
-          return ipv4Regex.test(str)
-        }
+    rules.ipv4 = {
+      msg: " must be IPv4",
+      validate: function(str) {
+        var ipv4Regex = new RegExp("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+        return ipv4Regex.test(str)
       }
+    }
 
-      rules.onlyA1_ = {
-        msg: " only accept a-b, A-b, 0-9, and '_'.",
-        validate: function(str) {
-          var regex = new RegExp("^[\u4E00-\u9FA5A-Za-z0-9_]+$")
-          return regex.test(str)
-        }
+    rules.onlyA1_ = {
+      msg: " only accept a-b, A-B, 0-9 and _.",
+      validate: function(str) {
+        var regex = new RegExp("^[\u4E00-\u9FA5A-Za-z0-9_]+$")
+        return regex.test(str)
       }
+    }
+
+    rules.asyncURL = {
+      msg: " must be url. (async)",
+      async: true,
+      validate: function(str) {
+        var that = this
+        var urlRegex = new RegExp("^http\:\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$")
+
+        return new Promise(function(resolve, reject) {
+
+          setTimeout(function() {
+
+            if (urlRegex.test(str)) {
+              resolve({
+                valid: true,
+                str: str
+              })
+            } else {
+              resolve({
+                valid: false,
+                msg: that.msg
+              })
+            }
+
+          })
+
+        })
+      }
+    }
+
+    rules.asyncipv4 = {
+      msg: " 必须是ipv4 异步版",
+      async: true,
+      validate: function(str) {
+        var that = this
+        var ipv4Regex = new RegExp("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+
+        return new Promise(function(resolve, reject) {
+
+          setTimeout(function() {
+
+            if (ipv4Regex.test(str)) {
+              resolve({
+                valid: true,
+                str: str
+              })
+            } else {
+              resolve({
+                valid: false,
+                msg: that.msg
+              })
+            }
+            
+          })
+
+        })
+      }
+    }
 
     return rules
-  }
-)
+  })
